5e6c4be98b02016ed124558f83c50b14fe36a272
Slerbamonsteri <75551384+Slerbamonsteri@users.noreply.github.com>
2023-03-25 03:11:13 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+
+fx_version "cerulean"
+
+games { 'rdr3' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author "Modified to RedM from FiveM by Slerbamonsteri#5636, can't remember who made it in the firstplace, but it's old script for FiveM, all credits to Original author eitherway."
+
+description "Allows player to look around with mouse movements when dead in RedM"
+
+client_scripts {
+  'cam.lua',
+}
+
